03cc785e5ae8ae59fba3cb7a1e327ee95e54b21e
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-07-25 23:59:50 +0530

    post- 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import StartPost from "./components/StartPost";
 function App() {
   return (
     <>
-      <div className='bg-yellow-100 mx-[28%]'>
+      <div className='mx-[28%]'>
         <StartPost />
       </div>
     </>
--- src/components/StartPost.jsx
+++ src/components/StartPost.jsx
@@ -1,40 +1,55 @@
 import { useState } from "react";
 import { CiImageOn } from "react-icons/ci";
-
-const Media = () => {
+import { BiSolidVideos } from "react-icons/bi";
+import { IoCalendar } from "react-icons/io5";
+import { GrArticle } from "react-icons/gr";
+const Media = ({ Icons, title, colors = "red" }) => {
   return (
     <>
-      <div className='flex justify-center  gap-1 items-center text-xl bg-red-100 '>
-        <CiImageOn className='w-[56px]  h-full' />
-        <span className='w-full '>Photo</span>
+      <div
+        className={`flex justify-center p-1 gap-2 items-center text-lg rounded-md`}
+      >
+        <Icons
+          className={`w-[45px] h-full text-${colors}-600`}
+          style={{ color: colors }}
+        />
+        <span className='w-full'>{title}</span>
       </div>
     </>
   );
 };
+
 const StartPost = () => {
   const [text, setText] = useState("");
   return (
     <>
-      <div className=' bg-blue-200'>
-        <div className='flex bg-green-300'>
+      <div className='  my-10 rounded-xl border'>
+        <div className='flex  gap-3 mx-5 py-2'>
           <div>
             <img
-              src='https://media.licdn.com/dms/image/C5603AQHW5p7nPuc-mg/profile-displayphoto-shrink_100_100/0/1621769516271?e=1695859200&v=beta&t=2VkVh3iqCuDd61i3jL8eoscf1T7cLfSkt9bJ7Im6Bug'
+              src='https://avatars.githubusercontent.com/u/84271800?v=4'
               alt='placeholder'
+              className='w-[60px] rounded-full'
             />
           </div>
           <input
             value={text}
+            placeholder='Start post ..'
             onChange={(e) => {
               setText(e.target.value);
             }}
+            className='w-full rounded-full text-lg border-[1px] border-gray-300  px-5'
           />
         </div>
-        <div className='m-2 flex w-full justify-around '>
-          <Media />
-          <Media />
-          <Media />
-          <Media />
+        <div className='m-2 pb-1 flex w-full justify-around '>
+          <Media title={"Photo"} Icons={CiImageOn} />
+          <Media title={"Video"} Icons={BiSolidVideos} />
+          <Media title={"Events"} Icons={IoCalendar} />
+          <Media
+            title={"Events"}
+            Icons={GrArticle}
+            className='text-green-400'
+          />
         </div>
       </div>
     </>
