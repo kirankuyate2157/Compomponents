4db46bd63abf973752fa33e6e7e30c4b818806c5
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-07-25 22:12:46 +0530

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className='bg-red-400 '>hey iam kiran kuyate </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/StartPost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StartPost = () => {
+  return <div></div>;
+};
+
+export default StartPost;
